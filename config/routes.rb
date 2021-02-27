3c7c6213d6d47c7aa741a49a36a0d594d4f38551
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-02-27 16:58:48 +0000

    updated profile show page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   resources :requests, only: [:edit, :update, :index]
   resources :skills, only: [:new, :create]
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show]
 
 end
