1f3c0eb743e0418b1a4146db0c5c721d90124dc9
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-03-04 21:34:12 +0000

    edited cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,14 +24,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import "instrument_card";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  toggleActiveClass();
-  toggleActiveOnClick();
 });
