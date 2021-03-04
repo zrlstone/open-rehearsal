ab331d7fee74ad4658b294a4a34134eeb540e154
Zak Stone <zrlstone@gmail.com>
2021-03-04 19:41:10 +0000

    added instrument icons and attached in seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
-  before_action :show_piano
 
   def home
-  end
-
-  def show_piano
-    @instrument = Instrument.find(114)
   end
 end
