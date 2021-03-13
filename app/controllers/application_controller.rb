15b547ff3d07cd5c1c807b5e172d6dc56949e833
Asia Ciesla <hello@ciesla.asia>
2021-03-13 15:17:58 +0000

    Request notification added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -10,4 +9,5 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :bio, :avatar])
   end
+
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,6 +4,11 @@ class RequestsController < ApplicationController
     @rehearsal = Rehearsal.all
     @incoming_requests = Request.joins(role: :rehearsal).where( roles: { user_id: nil }, rehearsals: { user_id: current_user.id } )
     @test = @incoming_requests
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { incoming_requests: @incoming_requests } }
+    end
   end
 
   def create
