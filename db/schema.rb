30ff3efd40e13e207de53d1c7d6fe81b4bf1c48c
Lydia Scadding <lydiascadding@gmail.com>
2021-03-13 12:20:53 +0000

    Put upcoming rehearsals on home page and changed navbar content

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_162058) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_rehearsals_on_user_id"
