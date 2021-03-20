b584bcc102bb0ddff600d77dc017b3178d1d252e
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-03-20 18:01:34 +0000

    updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 //= require flatpickr
+
 
 
 
@@ -29,6 +31,7 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import "../plugins/init_flatpickr";
+import "../plugins/init_finished_button";
 
 
 // Internal imports, e.g:
---
+++ app/javascript/plugins/init_finished_button.js
@@ -0,0 +1,15 @@
+  var NewContent='<div class="musical-notes"><div class="note-1">&#9835; &#9833;</div><div class="note-2">&#9833;</div><div class="note-3">&#9839; &#9834;</div><div class="note-4">&#9834;</div></div>'
+
+  var url = '';
+
+  $("#finished-button").click(function( event ) {
+    event.preventDefault();
+    $("#finished-button").append(NewContent);
+    $.ajax({
+      success: function () {
+          window.setTimeout(function () {
+              window.location = url;
+          }, 2000);
+      }
+    });
+  });
