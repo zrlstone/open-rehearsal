2b6948afd6f90928dec65815f133b96835895fef
Lydia Scadding <lydiascadding@gmail.com>
2021-03-20 12:49:43 +0000

    Request instance method

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,17 @@
 class Request < ApplicationRecord
   belongs_to :role
   belongs_to :user
+
+  def status
+    if accepted == true
+      'Confirmed'
+    elsif accepted == false
+      'Rejected'
+    elsif role.user == nil
+      'Pending'
+    end
+  end
+
+  scope :accepted, -> { where(accepted: true) }
+  scope :rejected, -> { where(accepted: false) }
 end
