b607c413651958c3f2373b3a8219b4ee612d6ef2
Lydia Scadding <lydiascadding@gmail.com>
2021-03-26 09:41:40 +0000

    Displays message if there are no pending requests for a rehearsal

--- app/javascript/plugins/init_requests_inbox.js
+++ app/javascript/plugins/init_requests_inbox.js
@@ -5,16 +5,27 @@ const initRequestsInbox = () => {
     $(".incoming-requests").hide();
     var activeTab = $(".rehearsal-panel .card-request:first").attr("rel");
     $("."+activeTab).show();
+    if ($("."+activeTab).length == 0) {
+      emptyInbox();
+    } else {
+      notEmptyInbox();
+    }
 
     $(".rehearsal-panel .card-request").click(function() {
 
       $(".incoming-requests").hide();
       var activeTab = $(this).attr("rel");
       $("."+activeTab).fadeIn();
+      if ($("."+activeTab).length == 0) {
+        emptyInbox();
+      } else {
+        notEmptyInbox();
+      }
       $(".rehearsal-panel .card-request").removeClass("active-request");
       $(this).addClass("active-request");
 
-      visitedPage = 1
+
+      //visitedPage = 1
 
 
     });
@@ -22,5 +33,13 @@ const initRequestsInbox = () => {
 
 
 };
+
+const emptyInbox = () => {
+  $(".no-requests").fadeIn();
+}
+
+const notEmptyInbox = () => {
+  $(".no-requests").hide();
+}
 
 export { initRequestsInbox };
